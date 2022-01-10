11e861f55af9ad1085f102c9f2a801d64a28c4bb
Myles Pribeg <mylespribeg@gmail.com>
2022-01-10 15:31:40 +0100

    Using message passing

--- Directory Lister (multithreading)/main.cpp
+++ Directory Lister (multithreading)/main.cpp
@@ -6,82 +6,113 @@
 #include <algorithm>
 #include <vector>
 #include <string>
+#include <condition_variable>
+#include <deque>
 
-struct Result
+const int NUM_THREADS = 10;
+
+template<class T>
+class MsgQueue
 {
-	std::vector<std::string> files;
-	std::vector<std::filesystem::path> dirs;
+	std::deque<T> queue;
+	std::condition_variable cond;
+	std::mutex mutex;
+public:
+	void send(T&& msg)
+	{
+		{
+			std::lock_guard<std::mutex> lck(mutex);
+			queue.push_front(std::move(msg));
+		}
+		cond.notify_one();
+	}
+	T recieve()
+	{
+		std::unique_lock<std::mutex> lck(mutex);
+		cond.wait(lck, [&] {return !queue.empty(); }); // this is equivalent to putting it in a while not empty loop
+		T msg = std::move(queue.back());
+		queue.pop_back();
+		return msg;
+	}
 };
 
-Result listDirectory(std::filesystem::path&& dir) 
+void listDirectoryServer(MsgQueue<std::filesystem::path>& dirQueue, MsgQueue<std::string>& fileQueue)
 {
-	Result result;
-
-	for (auto& p : std::filesystem::directory_iterator(dir))
+	while (1) 
 	{
-		//std::cout << p.path().filename().string() << '\n';
-		if (p.is_directory())
+		std::filesystem::path dir = dirQueue.recieve();
+		for (auto& p : std::filesystem::directory_iterator(dir))
 		{
-			result.dirs.push_back(p.path().string());
-			auto ftr = std::async(std::launch::async, &listDirectory, p.path().string());
-
+			if (p.is_directory())
+			{
+				dirQueue.send(std::filesystem::path(p.path()));
+			}
+			else
+			{
+				fileQueue.send(p.path().filename().string());
+			}
 		}
-		else
-		{
-			result.files.push_back(p.path().filename().string());
-		}
-
 	}
-	return result;
-
 }
+
+void printServer(MsgQueue<std::string>& nameQueue)
+{
+	while (1)
+	{
+		std::string name = nameQueue.recieve();
+		std::cout << name << std::endl;
+	}
+}
+
+//void listTree(std::filesystem::path&& rootDir)
+//{
+//
+//}
 
 int main() {
 	auto startTime = std::chrono::system_clock::now();
 	std::filesystem::path root("C:\\Users\\myles\\Desktop");
 
-	std::vector<std::filesystem::path> dirsToDo;
-	dirsToDo.push_back(root);
+	//std::vector<std::filesystem::path> dirsToDo;
+	//dirsToDo.push_back(root);
 
 	std::vector<std::string> files;
 
-	//listDirectory(std::move(root));
-	while (!dirsToDo.empty())
+	MsgQueue<std::filesystem::path> dirQueue;
+	MsgQueue<std::string> fileQueue;
+	dirQueue.send(std::move(root));
+
+	std::vector<std::future<void>> futures;
+	for (int i = 0; i < NUM_THREADS; i++)
 	{
-		std::vector<std::future<Result>> futures;
-		for (int i = 0; i < 16 && !dirsToDo.empty(); i++)
-		{
-			auto ftr = std::async(listDirectory, std::move(dirsToDo.back()));
-			dirsToDo.pop_back();
-			futures.push_back(std::move(ftr));
-		} 
+		auto ftr = std::async(std::launch::async, listDirectoryServer, std::ref(dirQueue), std::ref(fileQueue));
+		//dirsToDo.pop_back();
+		futures.push_back(std::move(ftr));
+	} 
+	futures.push_back(std::async(std::launch::async, printServer, std::ref(fileQueue)));
 
-		try
+	try
+	{
+		while (!futures.empty())
 		{
-			while (!futures.empty())
-			{
-				std::future ftr = std::move(futures.back());
-				futures.pop_back();
-				Result res = std::move(ftr.get()); //move constructor is implicitly generated
-				std::copy(res.files.begin(), res.files.end(), std::back_inserter(files));
-				std::copy(res.dirs.begin(), res.dirs.end(), std::back_inserter(dirsToDo));
-
-			}
+			std::future ftr = std::move(futures.back());
+			futures.pop_back();
+		}
 			
-			for (std::string s : files)
-			{
-				std::cout << s << std::endl;
-			}
-		}
-		catch (std::exception& e)
+		for (std::string s : files)
 		{
-			std::cout << e.what() << std::endl;
-		}
-		catch (...)
-		{
-			std::cout << "Unknown exception\n";
+			std::cout << s << std::endl;
 		}
 	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	catch (...)
+	{
+		std::cout << "Unknown exception\n";
+	}
+
 	auto endTime = std::chrono::system_clock::now();
 	auto durMs = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
 	std::cout << "\n\nCompleted in " << durMs << std::endl;
